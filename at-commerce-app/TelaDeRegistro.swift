af79bf319bff01584e60e0e1807929320db7f3dc
Andre Teixeira <andre.teixeira@compasso.com.br>
2026-06-21 15:44:45 -0300

    [ref] Separating view files

---
+++ at-commerce-app/TelaDeRegistro.swift
@@ -0,0 +1,91 @@
+//
+//  TelaDeRegistro.swift
+//  at-commerce-app
+//
+//  Created by Andre Luiz Goncalves Teixeira on 21/06/26.
+//
+
+import SwiftUI
+
+struct RegisterRequest: Codable {
+    let username: String
+    let password: String
+}
+
+struct TelaDeRegistro: View {
+    @State var usernameDigitado: String = ""
+    @State var passwordDigitada: String = ""
+    
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack {
+            Text("Cadastre-se")
+                .font(.title)
+                .bold()
+            Spacer()
+            TextField("Digite seu usuário", text: $usernameDigitado)
+                .textFieldStyle(.roundedBorder)
+                .autocorrectionDisabled(true)
+                .textInputAutocapitalization(.never)
+            
+            SecureField("Digite sua senha", text: $passwordDigitada)
+                .textFieldStyle(.roundedBorder)
+            
+            
+            Button(action: {
+                registrar()
+            }) {
+                Text("Enviar")
+                    .bold()
+                    .frame(maxWidth: 150)
+                    .padding()
+                    .background(Color.orange)
+                    .foregroundColor(.white)
+                    .cornerRadius(75)
+            }
+            
+            Spacer()
+        }
+    }
+    
+    func registrar() {
+        guard let url = URL(string: "http://localhost:3000/register") else { return }
+        let registerData = RegisterRequest(username: usernameDigitado, password: passwordDigitada)
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        do {
+            let jsonData = try JSONEncoder().encode(registerData)
+            request.httpBody = jsonData
+        } catch {
+            print("Error parsing data to JSON: \(error)")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("REQUEST ERROR: \(error.localizedDescription)")
+                return
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                print("Backend status code: \(httpResponse.statusCode)")
+                if httpResponse.statusCode == 200 || httpResponse.statusCode == 201 {
+                    DispatchQueue.main.async {
+                        dismiss()
+                    }
+                }
+            }
+            
+            if let data = data, let stringResponse = String(data: data, encoding: .utf8) {
+                print("Server response: \(stringResponse)")
+            }
+        }.resume()
+    }
+}
+
+#Preview {
+    TelaDeRegistro()
+}
