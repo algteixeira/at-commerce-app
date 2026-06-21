99854841610f346d69a3d5f1d89631993832ccf0
Andre Teixeira <andre.teixeira@compasso.com.br>
2026-06-21 11:52:56 -0300

    Initial Commit

---
+++ at-commerce-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  at-commerce-app
+//
+//  Created by Andre Luiz Goncalves Teixeira on 21/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ at-commerce-app/at_commerce_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  at_commerce_appApp.swift
+//  at-commerce-app
+//
+//  Created by Andre Luiz Goncalves Teixeira on 21/06/26.
+//
+
+import SwiftUI
+
+@main
+struct at_commerce_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
